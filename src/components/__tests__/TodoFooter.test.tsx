b578d9db4658d50f7c86d6e1c3ed4eda42c75519
Grigoriy Gavrilov <greeb.spb@gmail.com>
2025-03-26 22:24:02 +0500

    пофиксил ts ошибки

--- src/components/__tests__/TodoFooter.test.tsx
+++ src/components/__tests__/TodoFooter.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { vi } from 'vitest';
 import { TodoFooter } from '../TodoFooter';
-import { Filter } from '../../types/todo';
 
 describe('TodoFooter', () => {
   const setup = () => {
--- src/components/__tests__/TodoItem.test.tsx
+++ src/components/__tests__/TodoItem.test.tsx
@@ -1,5 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import { vi } from 'vitest';
 import { TodoItem } from '../TodoItem';
 import { Todo } from '../../types/todo';
 
